c90622bbe552c49861b1cc0436a8ac54d00f1e9f
Ditio123 <ditiorzl@gmail.com>
2022-02-08 11:46:02 +0700

    commit 1

---
+++ application/config/pagination.php
@@ -0,0 +1,33 @@
+<?php
+ // config
+ $config['base_url'] = 'http://localhost/ci-app/peoples/index';
+ $config['num_links'] = 5;
+
+ //styling
+ $config['full_tag_open'] ='<nav><ul class="pagination justify-content-center">';
+ $config['full_tag_close'] ='  </ul></nav>';
+
+ $config['first_link'] = 'First';
+ $config['first_tag_open'] ='<li class="page-item">';
+ $config['first_tag_open'] ='</li>';
+ 
+ $config['last_link'] = 'Last';
+ $config['last_tag_open'] ='<li class="page-item">';
+ $config['last_tag_open'] ='</li>';
+ 
+ $config['next_link'] = '&raquo';
+ $config['next_tag_open'] ='<li class="page-item">';
+ $config['next_tag_open'] ='</li>';
+ 
+ $config['prev_link'] = '&laquo';
+ $config['prev_tag_open'] ='<li class="page-item">';
+ $config['prev_tag_open'] ='</li>';
+ 
+ $config['cur_tag_open'] ='<li class="page-item active"><a class="page-link" href="#"';
+ $config['cur_tag_open'] ='</a></li>';
+ 
+ 
+ $config['num_tag_open'] ='<li class="page-item">';
+ $config['num_tag_open'] ='</li>';
+
+ $config['atributes'] = array('class' => 'page-link');
